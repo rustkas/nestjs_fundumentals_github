a83f2ef23b339256945848a8396a9dd84dd9f0eb
Anatolii Kosorukov <java1cprog@yandex.ru>
2022-10-30 08:15:26 +0700

    add a foler

---
+++ code/ilovcoffee/src/coffees/coffees.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+
+  // Implement Pagination with Query Parameters
+  // localhost:3000/coffees?limit=20&offset=10
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.coffeeService.findAll(paginationQuery);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.coffeeService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+    return this.coffeeService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeeService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.coffeeService.remove(id);
+  }
+}
---
+++ code/ilovcoffee/src/coffees/entities/coffee.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  JoinTable,
+  ManyToMany,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity() // sql table === 'coffee'
+export class Coffee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  brand: string;
+
+  @Column({ default: 0 })
+  recomendations: number;
+
+  @JoinTable()
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees, {
+    cascade: true,
+  })
+  flavors: Flavor[];
+}
